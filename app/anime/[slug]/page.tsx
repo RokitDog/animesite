a8406e719d0141f63a838eb867a649bb174a2e51
Rokitdog <bojanforgaming@hotmail.com>
2022-11-11 20:39:18 +0100

    Referer in fetch

--- app/anime/[slug]/page.tsx
+++ app/anime/[slug]/page.tsx
@@ -20,14 +20,14 @@ export default async function Page({ params, searchParams }: {
     searchParams: { episode: string },
   }) {
 
-    const episode = await fetchEpisode(params.slug, searchParams.episode)
+    const {Referer} = await fetchEpisode(params.slug, searchParams.episode)
     const animeDetails = await fetchAnimeDetails(params.slug);
     const totalEpisodes  = Array.from(Array(Number(animeDetails.totalEpisodes)).keys());
 
     return (
       <>
         <h2 className="text-2xl font-medium mt-[34px] capitalize">{animeDetails.animeTitle}, Episode: {searchParams.episode}</h2>
-        {episode.Referer ? <Iframe episode={episode.Referer} /> : <p className="text-4xl text-center my-[68px]">We are sorry, the episode that you are looking for doesnt exist...</p>}
+        {Referer ? <Iframe episode={Referer} /> : <p className="text-4xl text-center my-[68px]">We are sorry, the episode that you are looking for doesnt exist...</p>}
         <div className="flex items-center justify-between mt-[12px]">
         <Link className="bg-gray-500 p-3 hover:bg-gray-400 transition-all duration-150 ease-in-out" href={`/anime/${params.slug}?episode=${Number(searchParams.episode) - 1}`}>Previous Episode</Link>
         <Link className="bg-gray-500 p-3 hover:bg-gray-400 transition-all duration-150 ease-in-out" href={`/anime/${params.slug}?episode=${Number(searchParams.episode) + 1}`}>Next Episode</Link>
